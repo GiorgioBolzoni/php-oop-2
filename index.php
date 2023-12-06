e9509e3624af92ddb7bd7494153773fafe3fdc29
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-12-06 17:44:43 +0100

    aggiunti anche index e books.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include __DIR__ . "/Views/header.php";
+include __DIR__ . "/Model/Movie.php";
+?>
+<section class="container">
+    <h2>Movies</h2>
+    <div class="row gy-4">
+        <?php foreach ($movies as $movie) {
+            $movie->printCard();
+            // var_dump($movie);
+        } ?>
+    </div>
+</section>
+<?php
+include __DIR__ . "/Views/footer.php";
+?>
